404f24c4e301d3a23056cbcb99d8a869eff3956a
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-03-18 08:47:57 +0545

    add

--- JavaBean.java
+++
@@ -1,7 +0,0 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.io.Serializable;;
-
-public class JavaBean extends Canvas implements Serializable{
-    
-}
---
+++ LayoutManagement.java
@@ -0,0 +1,48 @@
+
+import java.awt.FlowLayout;
+
+import javax.swing.*;
+
+
+
+public class LayoutManagement extends JFrame{
+    JLabel Namelabel;
+    JLabel Password;
+    JTextArea Namearea;
+    JTextArea Passwordarea;
+
+    LayoutManagement(){
+        setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Layout Management");
+        // setLayout(new FlowLayout());
+        setLayout(new BoxLayout(this.getContentPane(),1));
+
+        Namelabel= new JLabel("Name :");
+        // Namelabel.setBounds(10,20,150,20);
+        add(Namelabel);
+
+        Namearea = new JTextArea("Enter Your Name");
+        // Namearea.setBounds(60,22,250,30);
+        add(Namearea);
+
+        Password= new JLabel("Password :");
+        // Password.setBounds(20,40,250,60);
+        add(Password);
+       
+        Passwordarea = new JTextArea("Enter Your Password");
+        // Password.setBounds(120,100,250,30);
+        add(Passwordarea);
+
+
+
+        setVisible(true);
+
+
+
+
+    }
+    public static void main(String[] args) {
+        new LayoutManagement();
+    }
+}
